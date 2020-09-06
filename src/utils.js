473484f30cfb4e3bcee76a19ef08d722529d092a
Marcin_Kloc <mkloc@genesis.com.mt>
2020-09-06 21:23:04 +0200

    update

--- src/utils.js
+++ src/utils.js
@@ -20,7 +20,7 @@ const getSourceFolder = () => {
 const convertCssVariables = mycss =>
   postcss([cssVariables()]).process(mycss).css
 
-const knownThemes = ['dark', 'halloween']
+const knownThemes = ['dark', 'halloween','christmas']
 
 const getHead = () => Cypress.$(parent.window.document.head)
 
